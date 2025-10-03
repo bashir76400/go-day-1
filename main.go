f089fca5a65157e6acc8c61a11948ab7c901d86a
LUPIN <Lupin_254>
2025-10-03 13:50:18 +0300

    fiihih

--- main.go
+++ main.go
@@ -7,5 +7,9 @@ func main() {
 	name := "bashir"
 	age := 24
 	cousre := "golang"
-	fmt.Println(greeting, "my name is", name, "and my age is", age, "i am learning", cousre)
+	calling(greeting, name, cousre, age)
+}
+
+func calling(greeting, name, course string, age int) {
+	fmt.Println(greeting, "my name is", name, "and my age is", age, "i am learning", course)
 }
