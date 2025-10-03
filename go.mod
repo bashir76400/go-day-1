beea9357f38f61f87c14b7f9a53165be925c6ad1
LUPIN <Lupin_254>
2025-10-03 13:27:47 +0300

    are you there

---
+++ go.mod
@@ -0,0 +1,3 @@
+module first_day
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	greeting := "hello "
+	name := "bashir"
+	age := 24
+	cousre := "golang"
+	fmt.Println(greeting, "my name is", name, "and my age is", age, "i am learning", cousre)
+}
